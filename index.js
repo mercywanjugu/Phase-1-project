d190b68f1c68e2a92ef13b966de5df75a5470336
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-25 12:46:25 -0400

    updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-//  const countryName = document.querySelector('#country-name');
-//  const countryImage = document.querySelector('#country-image');
-//  const countryDescription = document.querySelector('#country-description');
+ // const countryImage = document.querySelector('#country-image');
+ // const countryDescription = document.querySelector('#country-description');
 // // Target Description FORM
 //  const countryDescriptionForm= document.querySelector('#description-form');
 //  const countryEditDescription = document.querySelector('#description');
@@ -12,21 +11,14 @@
 // };
 
 
-//  // target review form
-//  const countryReviewForm = document.querySelector('#review-form');
-//  const countryReviewText = document.querySelector('#review');
+
 
 //  countryName.textContent = country.name,                   // country name
 //  countryImage.src = country.image_url,                     // country image
 //  countryDescription.textContent = country.description,     // country desc.
 //  countryEditDescription.value = country.description        //country description form
 
-//  // render country reviews
-//  for(let review of country.reviews){
-//      let countryReview = document.createElement('li');
-//     countryReview.textContent = review;
-//      countryReviewList.appendChild(countryReview);
-//  }
+
 //  // update description
 //  if (status){
 //      countryDescriptionForm.removeEventListener('submit',updateDescription, false);
@@ -65,47 +57,47 @@
 //     }
 // });
 
-// //  countryReviewForm.addEventListener('submit', (env) => {
-// //      env.preventDefault();
-// //      console.log(`review form ID: ${coutry.id}`)
-// //      console.log('---------------------------------')
-// //      if(countryReviewText.value !== ''){
-        
-// //          country.reviews.push(countryReviewText.value)
-// //          patchCountry(country)
-// //      } else{
-// //          alert('Review is empty string!!')
-// //      }
-// //  });
 
 
-
-//  function patchCountry(country){
-//  console.log(country, country.id)
-//  fetch(`https://www.travel-advisory.info/widget-no-js?countrycode=ISOCODE${country.id}`,
-//      {
-//          method: 'PATCH',
-//          headers: {'Content-Type': 'application/json'},
-//          body: JSON.stringify(country)
-//      })
-//      .then(response => response.json())
-//      .then(data => countryRender(data))
-//     .catch(err => console.log(`Error: ${err}`))
-//  };
+  function patchCountry(country){
+  console.log(country, country.id)
+  fetch(`https://www.travel-advisory.info/widget-no-js?countrycode=ISOCODE${AU}`,
+      {
+          method: 'PATCH',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(country)
+      })
+      .then(response => response.json())
+      .then(data => countryRender(data))
+     .catch(err => console.log(`Error: ${err}`))
+  };
 
 
 
 
  //comment box 
-const post = document.getElementById("post");
-post.addEventListener("click", function () {
-  let commentBoxValue = document.getElementById("comment-box").value;
+ const post = document.getElementById("post");
+ post.addEventListener("click", function () {
+   let commentBoxValue = document.getElementById("comment-box").value;
+ 
+   let li = document.createElement("li");
+   let text = document.createTextNode(commentBoxValue);
+   li.appendChild(text);
+   document.getElementById("unordered").appendChild(li);
+ 
+ });
+ //alert review 
+ post.addEventListener("click", ()=>alert('thanks for your review!'))
 
-  let li = document.createElement("li");
-  let text = document.createTextNode(commentBoxValue);
-  li.appendChild(text);
-  document.getElementById("unordered").appendChild(li);
-
-});
-//alert review 
-post.addEventListener("click", ()=>alert('thanks for your review!'))
+ //addeventlistener to countryButton
+ const countryButton=getElementById("countryButton")
+   let countryDescription=document.getElementById("country-description").value;
+ countryButton.addEventListener("click",function(){
+   let p=document.createElement('li');
+   let text=document.createTextNode(country-description);
+   p.appendChild(text);
+   document.getElementById("country-description").appendChild(p);
+ });
+// countryButton();
+ //countryDescription();
+ 
