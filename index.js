9d9a910b0bea9904f8f037f22c3d81bc08f8d958
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-30 11:42:32 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,103 +1,58 @@
- // const countryImage = document.querySelector('#country-image');
- // const countryDescription = document.querySelector('#country-description');
-// // Target Description FORM
-//  const countryDescriptionForm= document.querySelector('#description-form');
-//  const countryEditDescription = document.querySelector('#description');
-//  countryDescriptionForm.reset();
-//  // Remove Review Lists
-//  const countryReviewList = document.querySelector('#review-list');
-// while (countryReviewList.firstElementChild){
-//     countryReviewList.removeChild(countryReviewList.lastElementChild)
-// };
+let url = 'https://vast-refuge-29777.herokuapp.com/countries'
+const body = document.querySelector('#body')
+console.log(body.parentNode);
+body.parentNode.style.backgroundColor = '#f4f4f4';
+const comment = document.querySelector('textarea');
+const backUp = comment.getAttribute('placeholder');
+const btn = document.querySelector('btn');
+const clear = document.getElementById('clear');
+const submit = document.getElementById('submit');
+fetch(url)
+	.then(response => response.json())
+	.then(data => {
 
+		let html = ""
 
+		data.map((value) => {
+			html += `
+			 <div class = "col.md-9">
+			 <div class="card">
+                      <h4 class="title"><span>Name</span>: ${value.name}</h4>
+                      <h4 id="author"><span>Population</span>: ${value.Population}</h4>
+                   <img class="card-img-top" src="${value.imgUrl}" alt="Card image" style="width:50%">
+                 <div class="card-body">
+                 
+                   <p class="card-text">${value.CountryDescription}</p>
+                </div>
+             </div>
+			 </div>
+			`
+		})
 
+		document.getElementById('container').innerHTML = html
+	})
+	comment.onfocus = function(){
+		this.setAttribute('placeholder', '');
+		this.style.borderColor = '#333'
+		btn.style.display = 'block'
+	}
 
-//  countryName.textContent = country.name,                   // country name
-//  countryImage.src = country.image_url,                     // country image
-//  countryDescription.textContent = country.description,     // country desc.
-//  countryEditDescription.value = country.description        //country description form
+	comment.onblur = function(){
+		this.setAttribute('placeholder', backUp)
+		this.style.borderColor = '#aaa'
+	}
 
+	clear.onclick = function(){
+		btn.style.display = 'none';
+		comment.value = '';
+	}
 
-//  // update description
-//  if (status){
-//      countryDescriptionForm.removeEventListener('submit',updateDescription, false);
-//      status = !status
-//  } else {
-//      countryDescriptionForm.addEventListener('submit',updateDescription, false);
-//      status = !status
-//  }
-
-//  function updateDescription(env){
-//      env.preventDefault();        
-//      country.description = countryEditDescription.value;
-//      patchCountry(country)
-//  };
-
-
-//  // add new review    
-//  let status = false
-
-//  function counryRender(country){
-    
-//      // Country info.
-//      console.log("render country " + country.id)
-//      console.log('---------------------------------')
-//  }
-//  countryReviewForm.addEventListener('submit', (env) => {
-//     env.preventDefault();
-//     console.log(`review form ID: ${coutry.id}`)
-//     console.log('---------------------------------')
-//     if(countryReviewText.value !== ''){
-       
-//         country.reviews.push(countryReviewText.value)
-//         patchCountry(country)
-//     } else{
-//         alert('Review is empty string!!')
-//     }
-// });
-
-
-
-  function patchCountry(country){
-  console.log(country, country.id)
-  fetch(`https://www.travel-advisory.info/widget-no-js?countrycode=ISOCODE${AU}`,
-      {
-          method: 'PATCH',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify(country)
-      })
-      .then(response => response.json())
-      .then(data => countryRender(data))
-     .catch(err => console.log(`Error: ${err}`))
-  };
-
-
-
-
- //comment box 
- const post = document.getElementById("post");
- post.addEventListener("click", function () {
-   let commentBoxValue = document.getElementById("comment-box").value;
- 
-   let li = document.createElement("li");
-   let text = document.createTextNode(commentBoxValue);
-   li.appendChild(text);
-   document.getElementById("unordered").appendChild(li);
- 
- });
- //alert review 
- post.addEventListener("click", ()=>alert('thanks for your review!'))
-
- //addeventlistener to countryButton
- const countryButton=getElementById("countryButton")
-   let countryDescription=document.getElementById("country-description").value;
- countryButton.addEventListener("click",function(){
-   let p=document.createElement('li');
-   let text=document.createTextNode(country-description);
-   p.appendChild(text);
-   document.getElementById("country-description").appendChild(p);
- });
-// countryButton();
- //countryDescription();
- 
+	function alertBtn (){
+		alert('You are canceling your comment');
+	}
+	clear.addEventListener('click', alertBtn);
+	 function alertBtn1 (){
+	 	alert('Your comment has been submited');
+	 }
+	submit.addEventListener('click', alertBtn1);
+Footer
